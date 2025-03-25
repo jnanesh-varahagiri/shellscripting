85c7ffc66455f7d4cd84e801238a22f943e089fe
VD.Jnanesh Varahagiri <jnanesh@prodifyllp.com>
2025-03-25 22:57:41 +0530

    changes

--- 02-conditions.sh
+++ 02-conditions.sh
@@ -1,19 +1,51 @@
-#!/bin/bash
+# #!/bin/bash
 
-USERID=$(id -u)
+# USERID=$(id -u)
 
-if [ $USERID -ne 0 ] 
-then 
-  echo "You are not root user"
-  exit 1
-fi
 
-dnf list installed mysql
+# VALIDATE() {
+  
+#   if [ $1 -ne 0 ] 
+#   then 
+#     echo "mysql is not installed $2";
+#     return 0
+#   else
+#     echo "Mysql is insyalled $2"
+#     return 1
+#   fi
 
-if [ $? -ne 0] 
-then 
-  echo "mysql is not installed"
-  exit 1
-else
-  echo "mysql is installed"
-fi
+
+# }
+
+# if [ $USERID -ne 0 ] 
+# then 
+#   echo "You are not root user"
+#   INPUT=$(VALIDATE $? "Jnanesh")
+#   exit $INPUT
+# fi
+
+# echo $?;
+
+# dnf list installed mysql;
+
+# if [ $? -ne 0] 
+# then 
+#   echo "mysql is not installed"
+#   exit 1
+# else
+#   echo "mysql is installed"
+#   VALIDATE $? "Jnanesh"
+
+# fi
+
+
+add_numbers() {
+    local sum=$(( $1 + $2 ))
+    return $sum  # Returns only numbers from 0-255 (⚠️ not recommended for large numbers)
+}
+
+# Call function
+add_numbers 10 20;
+result=$?  # Capture function's exit code
+
+echo "Sum is: $result"
